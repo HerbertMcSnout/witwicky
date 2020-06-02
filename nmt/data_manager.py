b29f6bdeaa0cb1240a834b1abb529875677cd6c8
HerbertMcSnout <colinmcd0731@gmail.com>
2020-06-02 09:52:19 -0400

    Rework trees to binarize as left-child right-sibling

--- nmt/data_manager.py
+++ nmt/data_manager.py
@@ -279,7 +279,7 @@ class DataManager(object):
                 src_prsd = tree.parse(src_line)
                 trg_toks = trg_line.strip().split()
 
-                if 0 < src_prsd.weight() <= self.max_train_length and 0 < len(trg_toks) <= self.max_train_length:
+                if 0 < src_prsd.size() <= self.max_train_length and 0 < len(trg_toks) <= self.max_train_length:
                     num_lines += 1
                     if num_lines % 10000 == 0:
                         self.logger.info('    converting line {}'.format(num_lines))
