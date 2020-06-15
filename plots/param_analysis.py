34aedc079e6bb4aa447520336f8141452e8bd12d
HerbertMcSnout <colinmcd0731@gmail.com>
2020-06-15 16:31:42 -0400

    Added plotting code

---
+++ plots/param_analysis.py
@@ -0,0 +1,58 @@
+import sys
+sys.path.append('../')
+
+import nmt.structs as structs
+import nmt.configurations as config
+import plot_tree
+import torch
+import matplotlib.pyplot as plt
+from os.path import exists
+from os import listdir
+
+dpi = 1000
+
+sample = "(a (b (b3 (b4))) (c (d e f)) (g g1 g2) h (i (j k)))"
+
+saved_models_dir = "../nmt/saved_models"
+
+def cast_tree(t):
+  return t.fold_up(plot_tree.Tree)
+
+for model in listdir(saved_models_dir):
+  fp = "{0}/{1}/{1}.pth".format(saved_models_dir, model)
+
+  if exists(fp):
+    cnfg = config.get_config(model, getattr(config, model))
+    struct = cnfg["struct"]
+    tree = struct.parse(sample)
+    ks = struct.get_params(cnfg).keys()
+    m = torch.load(fp, map_location=torch.device("cpu"))
+    params = [m[k] for k in ks] # get param names
+
+    pe = cast_tree(struct.parse(sample).get_pos_embedding(cnfg["embed_dim"], params))
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,2,1, projection="polar")
+    ax2 = fig.add_subplot(1,2,2, projection="polar")
+    
+    plot_tree.plot_tree(ax1, pe, cm="PiYG")
+    plot_tree.plot_tree(ax2, pe.map(torch.norm), cm="cividis")
+
+    ax1.set_title("Position Embedding")
+    ax2.set_title("Frobenius Norm")
+
+    plt.tight_layout()
+    plt.savefig("pe-{}.png".format(model), dpi=dpi)
+    plt.close("all")
+
+  else:
+    print("{} doesn't exist".format(model))
+
+#def plot(title, *t, height=None):
+#    if len(t) != 1: t = torch.stack(list(t))
+#    elif len(t[0].size()) == 1: t = torch.stack(list(t)*(t[0].size()[0]//3))
+#    else: t = t[0]
+#    fig = plt.imshow(t, title=title)
+#    if height is not None: fig.update_layout(height=height)
+#    fig.show()
+
---
+++ plots/score_analysis.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('../')
+
+import nmt.structs as structs
+import nmt.configurations as config
+import torch
+import matplotlib.pyplot as plt
+from os.path import exists
+from os import listdir
+import numpy as np
+
+dpi = 1000
+lw = 0.8
+adjust_right = 0.63 # make room for legend
+
+saved_models_dir = "../nmt/saved_models"
+save_to = "scores.png"
+
+scores = {}
+
+for model in listdir(saved_models_dir):
+  bleu_fp = "{}/{}/bleu_scores.npy".format(saved_models_dir, model)
+  perp_fp = "{}/{}/dev_perps.npy".format(saved_models_dir, model)
+  scores[model] = {"bleu":[], "perp":[]}
+  if exists(bleu_fp): scores[model]["bleu"] = list(np.load(bleu_fp))
+  if exists(perp_fp): scores[model]["perp"] = list(np.load(perp_fp))
+
+max_epoch = max(max(len(scores[model]["bleu"]), len(scores[model]["perp"])) for model in scores)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+
+for model, score in scores.items():
+  perps = score["perp"]
+  epochs = list(range(1, len(perps) + 1))
+  marker = "" if len(perps) > 1 else "."
+  ax1.plot(epochs, perps, label=model, lw=lw, marker=marker)
+ax1.set(ylabel="Perplexity")
+plt.xlim(left=0, right=max_epoch)
+
+ax2 = fig.add_subplot(212)
+for model, score in scores.items():
+  bleus = score["bleu"]
+  epochs = list(range(1, len(bleus) + 1))
+  marker = "" if len(bleus) > 0 else "."
+  ax2.plot([0] + epochs, [0] + bleus, lw=lw, marker=marker)
+ax2.set(xlabel="Epoch", ylabel="BLEU")
+plt.xlim(left=0, right=max_epoch)
+
+fig.legend(loc=7)
+fig.tight_layout()
+fig.subplots_adjust(right=adjust_right)
+
+plt.savefig(save_to, bbox_inches="tight", dpi=dpi)
+plt.close("all")
