3118e7a02c3275952392af9986eec44756deb565
HerbertMcSnout <colinmcd0731@gmail.com>
2020-11-23 12:46:31 -0500

    Overhauled self-att plotting

--- plots/self_att.py
+++ plots/self_att.py
@@ -10,9 +10,10 @@ import nmt.utils as ut
 import nmt.structs.tree_utils as tu
 import plot_tree
 import torch
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import matplotlib
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
 import random
 import torch.nn.functional as F
@@ -20,33 +21,65 @@ import torch.nn.functional as F
 random.seed(ac.SEED)
 dpi = 400
 
+fontsize = 110
+nrows = 2
+
 device = ut.get_device()
 
-chosen_cmap = 'bwr'#'viridis'
+just_strengths = True
+plot_strengths = False
+reverse_cmap = True
 
-py2doc_sample = '(FUNCTIONDEF (NAME url slash cleaner) (ARGUMENTS (ARG (NAME url))) (BODY (RETURN (CALL (ATTRIBUTE (NAME re) (ATTR sub)) (ARGS STR STR (NAME url))))))'
-java2doc_sample = '(function (type (specifier public) (specifier static) (name void)) (name copy) (parameter_list (parameter (decl (type (name (name byte) index)) (name in))) (parameter (decl (type (name output stream)) (name out)))) (throws (name ioexception)) (block_content (call (name (name assert) (operator .) (name not null)) (argument_list (name in) (name string))) (call (name (name assert) (operator .) (name not null)) (argument_list (name out) (name string))) (call (name (name out) (operator .) (name write)) (argument_list (name in)))))' #'(function (type (specifier public) (name int)) (name size) parameter_list (block_content (return (call (name (name parameters) (operator .) (name size)) argument_list))))'
-en2vi_sample = '(S (S (NP (PRP$ My ) (NN family ) ) (VP (VBD was ) (RB not ) (ADJP (JJ poor ) ) ) ) (, , ) (CC and ) (S (NP (PRP myself ) ) (, , ) (NP (PRP I ) ) (VP (VBD had ) (ADVP (RB never ) ) (VP (VBN experienced ) (NP (NN hu@@ nger ) ) ) ) ) (. . ) )'
+relationship_reordering = [0, 3, 4, 5, 6, 7, 8, 1, 2]
 
-suffix = '_att_sin'
-py2doc_model = 'py2doc' + suffix
-java2doc_model = 'java2doc' + suffix
-en2vi_model = 'en2vi' + suffix
-en2tu_model = 'en2tu' + suffix
-en2ha_model = 'en2ha' + suffix
+name_suffix = '_mask_strengths' if just_strengths else '_heads'
 
-nrows = 3
+chosen_cmap = plt.get_cmap('Greens')
+if reverse_cmap:
+  chosen_cmap = chosen_cmap.reversed()
+
+en_sample = '(S (NP (PRP He ) ) (VP (VBZ is ) (NP (PRP\$ my ) (NN father ) ) ) (. . ) )'
+# German for 'He is my father.' -> 'Er ist mein Vater.' (according to Google Translate :P)
+de_sample = '(VROOT (S (PPER-SB Er) (VAFIN-HD ist) (NP-PD (PPOSAT-NK mein) (NN-NK Vater))) ($. .))'
 
 models = [
-  (en2vi_sample, en2vi_model),
-  (en2vi_sample, en2tu_model),
-  (en2vi_sample, en2ha_model),
-  (java2doc_sample, java2doc_model),
-  (py2doc_sample, py2doc_model)
+  (en_sample, 'en2vi_att_sin', 'en2vi_att_ast'),
+  (en_sample, 'en2ur_att_sin', 'en2ur_att_ast'),
+  (en_sample, 'en2tu_att_sin', 'en2tu_att_ast'),
+  (en_sample, 'en2ha_att_sin', 'en2ha_att_ast'),
+  (en_sample, 'en2de_att_sin_100k', 'en2de_att_ast'),
+  (de_sample, 'de2en_att_sin_100k', 'de2en_att_ast'),
+
+  (en_sample, 'en2vi17a2', 'en2vi_tree'),
+  (en_sample, 'en2ur17a2', 'en2ur_tree'),
+  (en_sample, 'en2tu17a2', 'en2tu_tree'),
+  (en_sample, 'en2ha17a2', 'en2ha_tree'),
+  (en_sample, 'en2de17a2_100k', 'en2de_tree'),
+  (de_sample, 'de2en17a2_100k', 'de2en_tree'),
 ]
 
-for sample, model_name in models:
-  print(model_name + '...', end='')
+def plot_mask_strength(ax, params, norm):
+  #ax.set_title('Mask Strength')
+
+  im_mtx = params['self_attn_weights'][:-1][relationship_reordering].detach()
+  ax_im = ax.imshow(im_mtx, cmap=chosen_cmap, norm=norm)
+  xticklabels = [i for i in range(im_mtx.size()[1])]
+  ax.set_xticks(xticklabels)
+  ax.set_xticklabels([i + 1 for i in xticklabels])
+  ax.set_xlabel("Attention Head")
+  yticklabels = tu.HEAD_NAMES[1:-1]
+  ax.set_yticks(range(len(yticklabels)))
+  ax.set_yticklabels([yticklabels[relationship_reordering[i]] for i in range(len(yticklabels))])
+  divider = make_axes_locatable(ax)
+
+  cax = divider.append_axes("right", size="6.25%", pad=0.1)
+  plt.colorbar(ax_im, cax=cax)
+
+
+
+for sample, model_name, save_name in models:
+  save_name += name_suffix
+  print(save_name + '...', end='')
   cnfg = config.get_config(model_name, getattr(config, model_name))
   
   struct = cnfg['struct']
@@ -78,37 +111,38 @@ for sample, model_name in models:
   
   att_min = att_mask.min().detach().cpu().item()
   att_max = att_mask.max().detach().cpu().item()
-  if att_max > 0 and att_min < 0:
-    att_max, att_min = max(att_max, -att_min), min(-att_max, att_min)
+  orig_range = att_min, att_max
+  #if att_max > 0 and att_min < 0:
+  #  att_max, att_min = max(att_max, -att_min), min(-att_max, att_min)
   norm = matplotlib.colors.Normalize(vmin=att_min, vmax=att_max)
+
+  if just_strengths:
+    fig = plt.figure(figsize=(4, 3))
+    plot_mask_strength(plt.gca(), params, norm)
+  else:
+    ncols = (num_heads + plot_strengths + nrows - 1)//nrows
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(3*ncols + 1, 3*nrows + 1))
   
-  ncols = num_heads//nrows + 1
-  fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(3*ncols + 1, 3*nrows + 1))
-  
-  ax = axs[0][0]
-  im_mtx = params['self_attn_weights'][:-1].detach()
-  ax_im = ax.imshow(im_mtx, cmap=chosen_cmap, norm=norm)
-  xticklabels = [i for i in range(im_mtx.size()[1])]
-  ax.set_xticks(xticklabels)
-  ax.set_xticklabels([i + 1 for i in xticklabels])
-  ax.set_xlabel("Attention Head")
-  #ax.set_ylabel("Node Relationship")
-  yticklabels = tu.HEAD_NAMES[1:-1]
-  ax.set_yticks(range(len(yticklabels)))
-  ax.set_yticklabels(yticklabels)
-  plt.colorbar(ax_im, ax=ax)
-  axs = sum([[x for x in row] for row in axs], [])
-  
-  for i, ax in enumerate(axs[1:]):
-    ax_im = ax.imshow(att_mask[i], cmap=chosen_cmap, norm=norm)
-    ax.set_xticks(range(len(words)))
-    ax.set_xticklabels(words, fontsize=140//len(words))
-    ax.set_yticks(range(len(words)))
-    ax.set_yticklabels(words, fontsize=140//len(words))
-    plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
-  
-  plt.tight_layout()
-  plt.savefig(f'plots/png/self-att-{model_name}.png', dpi=dpi)
+    #fig.suptitle(title, size='xx-large', weight='bold', va='top')
+    
+    try: axs = sum([[x for x in row] for row in axs], [])
+    except: pass
+
+    if plot_strengths:
+      ax = axs[0]
+      plot_mask_strength(ax, params, norm)
+
+    for i, ax in enumerate(axs[int(plot_strengths):]):
+      ax.set_title(f'Attention Head {i+1}')
+      ax_im = ax.imshow(att_mask[i], cmap=chosen_cmap, norm=norm)
+      ax.set_xticks(range(len(words)))
+      ax.set_xticklabels(words, fontsize=fontsize//len(words))
+      ax.set_yticks(range(len(words)))
+      ax.set_yticklabels(words, fontsize=fontsize//len(words))
+      plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
+    
+  plt.tight_layout(pad=1.25, w_pad=0.6, h_pad=0.6)
+  plt.savefig(f'plots/png/{save_name}.png', dpi=dpi, transparent=False, pad_inches=0.0,)
   plt.close('all')
-  print(' done')
+  print(f' done ({orig_range[0]:0.4f}, {orig_range[1]:0.4f})')
   
